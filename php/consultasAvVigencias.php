b59e0157a60b601d4f8e0d1f43b5a5bd60aaa3a6
Julian Varela <juva1121@gmail.com>
2014-04-25 21:00:06 -0500

    avance dinamico

--- php/consultasAvVigencias.php
+++ php/consultasAvVigencias.php
@@ -15,14 +15,14 @@ class consultasAvVigencias {
 */
     public function datosTablaAvanceVigencias(){
         $sql="SELECT v.vigencia, m.nivel_pdm, sfis.semaforo_seguimiento, sfin.recursos_programados, sfin.semaforo_seguimiento_financiero, sfin.recursos_ejecutados
-FROM matriz m, seguimiento_financiero sfin, seguimiento_fisico sfis, vigencias v, fila_matriz fm, clases c
-WHERE c.nombre = 'OG'
-AND c.id = m.clases_id
-AND m.id = fm.id_matriz
-AND sfis.id = fm.id_seguimiento_fisico
-AND sfin.id = fm.id_seguimiento_financiero
-AND v.id = id_vigencia
-";
+            FROM matriz m, seguimiento_financiero sfin, seguimiento_fisico sfis, vigencias v, fila_matriz fm, clases c
+            WHERE c.nombre = 'OG'
+            AND c.id = m.clases_id
+            AND m.id = fm.id_matriz
+            AND sfis.id = fm.id_seguimiento_fisico
+            AND sfin.id = fm.id_seguimiento_financiero
+            AND v.id = id_vigencia
+            ";
         
         $conexion = new ConectarBD(SERVIDOR, USUARIO, PASS, BD);
         $conexion->consultaSQL($sql);
